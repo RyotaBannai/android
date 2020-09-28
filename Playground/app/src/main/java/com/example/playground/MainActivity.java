0f8df68c6f824cbd948ed1b0037e2005e90e2154
RyotaBannai <ryotala0528@gmail.com>
2020-09-28 17:39:03 +0900

    add: learnt TextView class

--- Playground/app/src/main/java/com/example/playground/MainActivity.java
+++ Playground/app/src/main/java/com/example/playground/MainActivity.java
@@ -11,11 +11,14 @@ import android.widget.LinearLayout;
 import android.os.Bundle;
 import android.widget.TextView;
 import android.view.ViewGroup.LayoutParams;
-import android.view.ViewGroup;
+import android.view.Gravity;
 import android.graphics.Color;
+import android.graphics.Typeface;
+
 
 public class MainActivity extends ComponentActivity {
-    private final int WRAP_CONTENT = ViewGroup.LayoutParams.WRAP_CONTENT;
+    private final int WRAP_CONTENT = LayoutParams.WRAP_CONTENT; //
+    private final int MATCH_PARENT = LayoutParams.MATCH_PARENT; //
 
     @Override
 //    public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -23,17 +26,26 @@ public class MainActivity extends ComponentActivity {
         super.onCreate(icicle);
 //        setContentView(android.R.string.VideoView_error_button);
 //        getWindow().setTitle(getResources().getText(R.string.app_name));
+
+        LinearLayout linearLayout = new LinearLayout(this);
+        linearLayout.setOrientation(LinearLayout.VERTICAL);
+
         TextView tv = new TextView(this);
         tv.setText("Hello", TextView.BufferType.EDITABLE);
         tv.setTextColor(Color.RED);
-        LinearLayout linearLayout = new LinearLayout(this);
-        linearLayout.setOrientation(LinearLayout.VERTICAL);
+        tv.setBackgroundColor(Color.LTGRAY);
+        tv.setTextSize(20.0f);
+        tv.setTypeface(Typeface.MONOSPACE);
 
         setContentView(linearLayout);
         TextView tv2 = new TextView(this);
         tv2.setText("Color RGB[127, 127, 0]");
         tv2.setTextColor(Color.rgb(127, 127, 0));
-        linearLayout.addView(tv, new LinearLayout.LayoutParams(WRAP_CONTENT, WRAP_CONTENT));
-        linearLayout.addView(tv2, new LinearLayout.LayoutParams(WRAP_CONTENT, WRAP_CONTENT));
+        tv2.setHeight(100);
+        tv2.setBackgroundColor(Color.DKGRAY);
+        tv2.setPadding(5, 5, 5, 5);
+        tv2.setGravity(Gravity.CENTER); // centering both in vertically and horizontally.
+        linearLayout.addView(tv, new LinearLayout.LayoutParams(MATCH_PARENT, WRAP_CONTENT)); // width, height
+        linearLayout.addView(tv2, new LinearLayout.LayoutParams(WRAP_CONTENT, WRAP_CONTENT)); // WRAP_CONTENT は 自動調整はしないで最低限表示に必要なスペースを確保する
     }
 }
