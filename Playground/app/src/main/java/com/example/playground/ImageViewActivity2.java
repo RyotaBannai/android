02e7b4f9d1f5f1362d6264208edceb3c990e3266
RyotaBannai <ryotala0528@gmail.com>
2020-09-29 23:50:55 +0900

    add: display image in simpler way.

---
+++ Playground/app/src/main/java/com/example/playground/ImageViewActivity2.java
@@ -0,0 +1,40 @@
+package com.example.playground;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+
+/**
+ *  Simpler way to display image file
+ * */
+public class ImageViewActivity2 extends Activity {
+    private final int WC = ViewGroup.LayoutParams.WRAP_CONTENT;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // get image
+        ImageView imgView = new ImageView(this);
+        imgView.setImageResource(R.drawable.daredevil);
+
+        // title
+        TextView tv = new TextView(this);
+        tv.setText("I'm the DareDevil!!");
+
+        LinearLayout linearLayout = new LinearLayout(this);
+        linearLayout.setOrientation(LinearLayout.VERTICAL);
+        linearLayout.addView(tv, createParam(WC, WC)); // no need of params for layout. only view needs params.
+        linearLayout.addView(imgView, createParam(WC, WC));
+        setContentView(linearLayout);
+    }
+
+    private LinearLayout.LayoutParams createParam(int w, int h) {
+        return new LinearLayout.LayoutParams(w, h);
+    }
+}
