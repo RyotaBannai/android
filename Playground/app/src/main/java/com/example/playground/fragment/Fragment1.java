5dd635a29a2157a86fc84530ecd8aeb4cb8a5566
RyotaBannai <ryotala0528@gmail.com>
2020-10-01 22:23:14 +0900

    add: learnt Fragment

---
+++ Playground/app/src/main/java/com/example/playground/fragment/Fragment1.java
@@ -0,0 +1,35 @@
+package com.example.playground.fragment;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+
+import com.example.playground.R;
+
+public class Fragment1 extends Fragment {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        /*
+        * システムが Fragment を作成したとき
+        * */
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        /*
+        * Fragment の UI が描画されるタイミングで呼び出される
+        * */
+        super.onCreateView(inflater, container, savedInstanceState);
+        return inflater.inflate(R.layout.fragment1, container, false);
+    }
+
+    // onPause()
+    /*
+    * Fragmentが停止するときによばれる。Fragmentで変更されたステータスの保存はここで行う。
+    * */
+}
