0af2d105c10ac14ec71a6151f972bc958548bc03
RyotaBannai <ryotala0528@gmail.com>
2020-10-06 23:04:51 +0900

    add: add styleable and use it in custom layout. if you inherit View and implement onDraw and other methods, it's called custom view.

--- Playground/app/src/main/java/com/example/playground/CustomViewActivity.java
+++ Playground/app/src/main/java/com/example/playground/CustomViewActivity.java
@@ -9,8 +9,8 @@ public class CustomViewActivity extends Activity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.painting_layout);
-//        setContentView(R.layout.custom_view_activity_main);
+//        setContentView(R.layout.painting_layout);
+        setContentView(R.layout.custom_view_activity_main);
         // setContentView(new CustomLayout(getApplicationContext(), null)); // これも可
     }
 }
--- Playground/app/src/main/java/com/example/playground/customView/CustomLayout.java
+++ Playground/app/src/main/java/com/example/playground/customView/CustomLayout.java
@@ -9,27 +9,57 @@ import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+/* Add two below module to use styleable */
+import android.content.res.TypedArray;
+import android.util.AttributeSet;
+
 import com.example.playground.R;
 
 import javax.annotation.Nullable;
 
+/*
+* ref: custom view http://ojed.hatenablog.com/entry/2015/12/05/161013#layout
+* */
 public class CustomLayout extends LinearLayout implements View.OnClickListener {
     private TextView viewText;
     private EditText editText;
+    /* Add two below module to use styleable */
+    private boolean mEnableToSend;
+    private int mButtonName;
 
     public CustomLayout(Context context, @Nullable AttributeSet attr) {
         super(context, attr);
-
         View layout = LayoutInflater.from(context).inflate(R.layout.custom_layout, this); // need to be inflate!!!
-        viewText = (TextView) layout.findViewById(R.id.textView);
-        editText = (EditText) layout.findViewById(R.id.editText);
+
+        /* This TypedArray should be recycled after use with #recycle() */
+        TypedArray a = context.obtainStyledAttributes(attr, R.styleable.CustomLayout, 0, 0);
+
+        try {
+            mEnableToSend = a.getBoolean(R.styleable.CustomLayout_EnableToSend, true);
+            mButtonName = a.getInteger(R.styleable.CustomLayout_ButtonName, 0);
+        } finally {
+            a.recycle();
+        }
         Button button = (Button) layout.findViewById(R.id.button);
         button.setOnClickListener(this);
+        switch (mButtonName) {
+            default:
+            case 0:
+                button.setText("send");
+            case 1:
+                button.setText("save");
+            case 2:
+                button.setText("delete");
+        }
+
+        viewText = (TextView) layout.findViewById(R.id.textView);
+        editText = (EditText) layout.findViewById(R.id.editText);
     }
 
     public void onClick(View view) {
         if (view.getId() == R.id.button) {
-            viewText.setText(editText.getText());
+            if (mEnableToSend)
+                viewText.setText(editText.getText());
             editText.setText(null);
         }
     }
