4ce2c13ff14e1943ff6fdd5c77d4ba9f3bc11a80
RyotaBannai <ryotala0528@gmail.com>
2020-10-08 20:29:32 +0900

    add: data binding(add an example of view holder)

--- Playground/app/src/main/java/com/example/playground/adapter/DataBindingAdapter.java
+++ Playground/app/src/main/java/com/example/playground/adapter/DataBindingAdapter.java
@@ -1,5 +1,6 @@
 package com.example.playground.adapter;
 
+import android.app.Activity;
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,12 +12,15 @@ import androidx.annotation.Nullable;
 import androidx.databinding.DataBindingUtil;
 
 import com.example.playground.R;
+import com.example.playground.ViewHolder;
 import com.example.playground.data.User;
 import com.example.playground.databinding.BindingMainActivityBinding;
 
 import java.util.ArrayList;
 
 public class DataBindingAdapter extends ArrayAdapter<User> {
+//    ViewHolder holder = null;
+
     public DataBindingAdapter(Context context, ArrayList<User> users) {
         super(context, 0, users);
     }
@@ -25,6 +29,10 @@ public class DataBindingAdapter extends ArrayAdapter<User> {
     @Override
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
 //        return super.getView(position, convertView, parent);
+
+        /*
+         * View との紐づけに Data Binding を用いることで View Holder が不要になりスッキリ書ける.
+         * */
         BindingMainActivityBinding binding;
         if (convertView == null) {
             LayoutInflater inflater = LayoutInflater.from(getContext());
@@ -37,5 +45,22 @@ public class DataBindingAdapter extends ArrayAdapter<User> {
         }
         binding.setUser(getItem(position));
         return binding.getRoot();
+
+        /*
+         * View Holder を使う場合
+         * */
+//        LayoutInflater inflater = (LayoutInflater) getContext().getSystemService(Activity.LAYOUT_INFLATER_SERVICE);
+//        if(convertView == null){
+//            convertView = inflater.inflate(R.layout.binding_main_activity, null);
+//            holder = new ViewHolder(convertView);
+//            convertView.setTag(holder);
+//        }
+//        else {
+//            holder = (ViewHolder) convertView.getTag();
+//        }
+//
+//        holder.name = "NAME";
+//        holder.age = 10;
+
     }
 }
