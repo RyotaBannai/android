d44bcfffa0eb1dc00e5bc3f214f863d0bb7bcc98
RyotaBannai <ryotala0528@gmail.com>
2020-10-10 18:54:56 +0900

    add: finished up http reuqest

--- Playground/app/src/main/java/com/example/playground/HttpRequest/HttpRequestActivity.java
+++ Playground/app/src/main/java/com/example/playground/HttpRequest/HttpRequestActivity.java
@@ -6,12 +6,15 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.playground.HttpRequest.pojo.MultipleResources;
+import com.example.playground.HttpRequest.pojo.User;
+import com.example.playground.HttpRequest.pojo.UserList;
 import com.example.playground.R;
 
 import java.util.List;
@@ -20,6 +23,10 @@ import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 import retrofit2.internal.EverythingIsNonNull;
+
+/*
+ * reference: https://www.journaldev.com/13639/retrofit-android-example-tutorial#retrofit-android
+ * */
 
 public class HttpRequestActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -49,6 +56,10 @@ public class HttpRequestActivity extends AppCompatActivity implements View.OnCli
         nextButton.setOnClickListener(this);
         backButton = findViewById(R.id.back);
         backButton.setOnClickListener(this);
+
+        // test
+        User user = new User("Morpheus", "leader");
+        createUser(user);
     }
 
     @RequiresApi(api = Build.VERSION_CODES.N)
@@ -85,17 +96,68 @@ public class HttpRequestActivity extends AppCompatActivity implements View.OnCli
                 _totalPages = resources.totalPages;
                 List<MultipleResources.Datum> datumList = resources.data;
 
-                displayResponse.append(text).append(" Page \n").append(total).append(" Total\n").append(totalPages).append(" Total Pages\n");
+                displayResponse.append(text).append(" Page\n")
+                        .append(total).append(" Total\n")
+                        .append(totalPages).append(" Total Pages\n");
                 for (MultipleResources.Datum datum : datumList) {
-                    displayResponse.append(datum.id).append(" ").append(datum.name).append(" ").append(datum.pantoneValue).append(" ").append(datum.year).append("\n");
+                    displayResponse.append(datum.id).append(" ")
+                            .append(datum.name).append(" ")
+                            .append(datum.pantoneValue).append(" ")
+                            .append(datum.year).append("\n");
                 }
-
                 responseText.setText(displayResponse);
             }
 
             @EverythingIsNonNull
             @Override
             public void onFailure(Call<MultipleResources> call, Throwable t) {
+                call.cancel();
+            }
+        });
+    }
+
+    private void createUser(User user) {
+        Call<User> call = apiInterface.createUser(user);
+        call.enqueue(new Callback<User>() {
+            @EverythingIsNonNull
+            @Override
+            public void onResponse(Call<User> call, Response<User> response) {
+                User user = response.body();
+                assert user != null;
+                Toast.makeText(getApplicationContext(), user.name + " " + user.job + " " + user.id + " " + user.createAt, Toast.LENGTH_SHORT).show();
+            }
+
+            @EverythingIsNonNull
+            @Override
+            public void onFailure(Call<User> call, Throwable t) {
+                call.cancel();
+            }
+        });
+    }
+
+    private void createUserFromForm(String name, String job) {
+        Call<UserList> call3 = apiInterface.doCreateUserWithField(name, job);
+        call3.enqueue(new Callback<UserList>() {
+            @EverythingIsNonNull
+            @Override
+            public void onResponse(Call<UserList> call, Response<UserList> response) {
+                UserList userList = response.body();
+                assert userList != null;
+                Integer text = userList.page;
+                Integer total = userList.total;
+                Integer totalPages = userList.totalPages;
+                List<UserList.Datum> datumList = userList.data;
+                Toast.makeText(getApplicationContext(), text + " page\n" + total + " total\n" + totalPages + " totalPages\n", Toast.LENGTH_SHORT).show();
+
+                for (UserList.Datum datum : datumList) {
+                    Toast.makeText(getApplicationContext(), "id : " + datum.id + " name: " + datum.first_name + " " + datum.last_name + " avatar: " + datum.avatar, Toast.LENGTH_SHORT).show();
+                }
+
+            }
+
+            @EverythingIsNonNull
+            @Override
+            public void onFailure(Call<UserList> call, Throwable t) {
                 call.cancel();
             }
         });
