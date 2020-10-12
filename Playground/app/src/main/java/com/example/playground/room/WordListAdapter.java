678ad222e940e44826e4e0c02871b56aa1d69302
RyotaBannai <ryotala0528@gmail.com>
2020-10-12 19:47:55 +0900

    add: create view in room project in pregress

---
+++ Playground/app/src/main/java/com/example/playground/room/WordListAdapter.java
@@ -0,0 +1,40 @@
+package com.example.playground.room;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.playground.R;
+import com.example.playground.room.db.entity.Word;
+
+import java.util.List;
+
+public class WordListAdapter extends RecyclerView.Adapter<WordListAdapter.WordViewHolder> {
+    static class WordViewHolder extends RecyclerView.ViewHolder {
+        private final TextView wordItemView;
+
+        private WordViewHolder(View itemView) {
+            super(itemView);
+            wordItemView = itemView.findViewById(R.id.textView);
+        }
+    }
+
+    private final LayoutInflater mInflater;
+    private List<Word> mWords;
+
+    WordListAdapter(Context context) {
+        mInflater = LayoutInflater.from(context);
+    }
+
+    @NonNull
+    @Override
+    public WordViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View itemView = mInflater.inflate(R.layout.recyclerview_item, parent, false);
+        return new WordViewHolder(itemView);
+    }
+}
