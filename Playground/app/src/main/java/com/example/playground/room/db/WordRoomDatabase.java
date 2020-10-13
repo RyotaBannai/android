cca60ce05bd7f31ef3dfa93bd30d3c64c9d7aa92
RyotaBannai <ryotala0528@gmail.com>
2020-10-13 13:13:11 +0900

    add: seed/ populates database

--- Playground/app/src/main/java/com/example/playground/room/db/WordRoomDatabase.java
+++ Playground/app/src/main/java/com/example/playground/room/db/WordRoomDatabase.java
@@ -2,9 +2,11 @@ package com.example.playground.room.db;
 
 import android.content.Context;
 
+import androidx.annotation.NonNull;
 import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
+import androidx.sqlite.db.SupportSQLiteDatabase;
 
 import com.example.playground.room.db.dao.WordDao;
 import com.example.playground.room.db.entity.Word;
@@ -30,11 +32,39 @@ public abstract class WordRoomDatabase extends RoomDatabase {
                     INSTANCE = Room.databaseBuilder(
                             context.getApplicationContext(),
                             WordRoomDatabase.class,
-                            "word_database"
-                    ).build();
+                            "word_database")
+                            .addCallback(sRoomDatabaseCallback)
+                            .build();
                 }
             }
         }
         return INSTANCE;
+    }
+
+    /*
+     * You will add data in two ways:
+     * - Add some data when the database is opened, and
+     * - add an Activity for adding words.
+     * */
+
+    private static RoomDatabase.Callback sRoomDatabaseCallback = new RoomDatabase.Callback() {
+        @Override
+        public void onOpen(@NonNull SupportSQLiteDatabase db) { // run when database  is opened
+            super.onOpen(db);
+
+            // If you want to keep data through app restarts,
+            // comment out the following block
+            databaseWriteExecutor.execute(() -> {
+                WordDao dao = INSTANCE.wordDao();
+                dao.deleteAll();
+
+                // Populate the database in the background.
+                // If you want to start with more words, just add them.
+                Word word = new Word("Hello");
+                dao.insert(word);
+                word = new Word("Hello");
+                dao.insert(word);
+            });
+        }
     }
 }
